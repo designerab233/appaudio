e8d9e2becc200b7c5ea16ec1e8130022764baccc
designerab233 <designerab2@gmail.com>
2025-09-11 12:06:36 +0100

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,23 @@
 import streamlit as st
-
 import whisper
-
 import tempfile
 
 # --- En-tête style inversé ---
-
 st.markdown(
-
     """
-
     <div style="
-
-        background-color:white;
-
-        color:black;
-
+        background-color:black;
+        color:white;
         padding:20px;
-
         text-align:center;
-
         font-size:28px;
-
         font-weight:bold;
-
         letter-spacing:2px;
-
     ">
-
         ✨ Created by Abdessamad Karim ✨
-
     </div>
-
     """,
-
     unsafe_allow_html=True
-
 )
 
 st.set_page_config(page_title="Transcription Audio", layout="centered")
@@ -43,9 +25,7 @@ st.set_page_config(page_title="Transcription Audio", layout="centered")
 st.title("🎙️ Transcription Audio avec Whisper")
 
 @st.cache_resource
-
 def load_model():
-
     return whisper.load_model("base")
 
 model = load_model()
@@ -53,24 +33,16 @@ model = load_model()
 uploaded_file = st.file_uploader("Choisissez un fichier audio", type=["mp3", "wav", "m4a"])
 
 if uploaded_file is not None:
-
     st.audio(uploaded_file)
 
     # Sauvegarde temporaire
-
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as tmp:
-
         tmp.write(uploaded_file.read())
-
         temp_audio = tmp.name
 
     st.write("⏳ Transcription en cours...")
-
     result = model.transcribe(temp_audio, fp16=False)
-
     st.success("✅ Transcription terminée")
 
     st.subheader("Texte transcrit :")
-
     st.write(result["text"])
-
